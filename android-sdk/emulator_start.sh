5338a6f282b047807a4a25d6211e27dbdf713056
fihser.liu <fisher.liu@egistec.com>
2017-10-15 12:35:50 +0800

    emulator name TestAVD_armeabi-v7a

--- android-sdk/emulator_start.sh
+++ android-sdk/emulator_start.sh
@@ -1,5 +1,6 @@
-EMULATOR_NAME=testAVD
-echo "Start emulator ..."
+EMULATOR_NAME=TestAVD_armeabi-v7a
+echo "Creating emulator "$EMULATOR_NAME
 echo "no" | avdmanager create avd -n ${EMULATOR_NAME} -k "system-images;android-24;default;armeabi-v7a"
+echo "Start emulator ..."
 emulator64-arm -avd ${EMULATOR_NAME} -noaudio -no-boot-anim -no-window -accel on
 
